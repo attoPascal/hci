f4f289789cf590a34813da9c604573a4b7aa5f39
Sandra Markhart <sandra.nina.ma@gmail.com>
2014-04-22 22:41:46 +0200

    Persönlicher Plan teilweise aus dem .csv-File ausgelesen und ein paar Sachen verlinkt.

--- plan/index.php
+++ plan/index.php
@@ -2,13 +2,13 @@
 <html lang="de">
   <head>
     <meta charset="utf-8"> 
-    <title>eCurriculum</title>
+    <title>Persönlicher Plan</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles -->
-    <link href="css/ecurriculum.css" rel="stylesheet">
+    <link href="../css/ecurriculum.css" rel="stylesheet">
   </head>
 
   <body>
@@ -17,7 +17,7 @@
       <div class="container">
 	<div class="row">
 	  <!-- Page Title -->
-	  <div class="col-md-6"><h1 class="page-header-ecurriculum-title">eCurriculum</h1></div>
+	  <div class="col-md-6"><h1 class="page-header-ecurriculum-title">Persönlicher Plan - Max Mustermann (a1234567)</h1></div>
 	  <!-- Page Logo -->
 	  <div class="col-md-6">
 	    <div class="page-header-ecurriculum-logo">
@@ -40,10 +40,13 @@
           </button>
         </div>
         <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="/">Bachelor Medieninformatik</a></li>
+          </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="plan/">
+            <li><a href="../">
             <span class="glyphicon glyphicon-arrow-right"></span>
-            Persönlicher Plan</a></li>
+            eCurriculum</a></li>
           </ul>
         </div> <!--/.nav-collapse -->
       </div> <!-- /container -->
@@ -51,34 +54,55 @@
     
     <!-- Content -->
     <div class="container">
-      <div class="row">
-	<div class="col-md-12">
-	  <h1>Bachelor</h1>
-	    <ul class="nav nav-pills nav-stacked">
-	      <li class="disabled"><a href="#">Bioinformatik</a></li>
-	      <li><a href="bachelor/index.php?studienrichtung=medieninformatik">Medieninformatik</a></li>
-	      <li class="disabled"><a href="#">Medizininformatik</a></li>
-	      <li class="disabled"><a href="#">Scientific Computing</a></li>
-	    </ul>
+      <table class="table table-bordered">
+    
+<?php
+$plan = array();
+if (($handle = fopen("eCurriculum_Bachelor_Informatik_Plan.csv", "r")) !== FALSE) {
+    $key = 0;    // Set the array key.
+    while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+        $count = count($data);  //get the total keys in row
+        //insert data to our array
+        for ($i=0; $i < $count; $i++) {
+            $plan[$key][$i] = $data[$i];
+        }
+        $key++;
+    }
+    fclose($handle);    //close file handle
+} 
 
-	  <h1>Master</h1>
+for ($i=1; $i < 7; $i++)
+  printModulSemester($plan, $i);
 
-	    <ul class="nav nav-pills nav-stacked">
-	      <li class="disabled"><a href="#">Informatikdidaktik</a></li>
-	      <li class="disabled"><a href="#">Medieninformatik</a></li>
-	      <li class="disabled"><a href="#">Medizinische Informatik</a></li>
-	      <li class="disabled"><a href="#">Scientific Computing</a></li>
-	    </ul>
-	  </div>
-	
-     
-      </div> <!-- /row -->
+?>
+      </table>
+
     </div> <!-- /container -->
     
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
+
+<?php
+  function printModulSemester($plan, $semester) {
+    $count = count($plan);
+    echo "<tr>";
+    echo "<th>".$semester.". Semester</th>";
+      for ($i=1; $i < $count; $i++) {
+	if (($plan[$i][6] == "Medieninformatik") AND ($plan[$i][7] == $semester)) {
+	  if ($plan[$i][8] AND  $plan[$i][8] < 5)
+	    echo "<td bgcolor=#e7e7e7>".
+	      $plan[$i][1]."\n 
+	      <span class='glyphicon glyphicon-ok'></span>
+	      <span class='badge'> ".$plan[$i][8]."</span></td>";
+	  else
+	    echo "<td>".$plan[$i][1]."</td>";
+	}
+    }
+    echo "</tr>";
+  } 
+?>
